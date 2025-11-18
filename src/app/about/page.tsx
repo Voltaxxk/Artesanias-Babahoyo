dcf00b0523b001bae52d551e4761059f65084ad7
Cristhian <alfredogarzon1234@hotmail.com>
2025-11-18 10:31:45 -0500

    [ADD] ficheros iniciales

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const aboutUs = () => {
+  return (
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
+  )
+}
+
+export default aboutUs
